7e0b63b2d572ae767c1fd14af8676396840a99a8
logansmith <logansmith@LEM231-30>
2023-03-08 18:12:37 -0800

    Add project files.

---
+++ COMP003A.FinalExam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.FinalExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
