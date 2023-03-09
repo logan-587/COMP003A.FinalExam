3cf7aed9f8c5d0004359103e4ef1fb32c780dd4e
Logan Smith <logansmith@my.whccd.edu>
2023-03-08 18:28:29 -0800

    Add To Program

--- COMP003A.FinalExam/Program.cs
+++ COMP003A.FinalExam/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace COMP003A.FinalExam
+﻿/*
+ * Author: Logan Smith
+ * Course: COMP-003A 
+ * Assignment: Final Project
+ * Purpose: An Intake Form for 
+ */
+namespace COMP003A.FinalExam
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+
+
+
+        ///<summary>
+        ///Prints title for Form
+        ///</summary>
+        ///<param name="intake">Bool input for Intake (true) mode or print (false) mode.</param>
+        static void Logo(bool intake)
+        {
+            Console.WriteLine("\n".PadRight(102,'*'));
+            Console.WriteLine("COMPANY NAME");
+            Console.WriteLine("INTAKE FORM NAME");
+            
+            if(intake)
+            {
+                Console.WriteLine("\nNew User Profile:\n");
+            } else {
+                Console.WriteLine("\nProfile Summary:\n");
+            }
+        }
+
+
+
     }
 }
