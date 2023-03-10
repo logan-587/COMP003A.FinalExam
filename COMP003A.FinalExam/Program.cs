11bbf44e0d3b3fc8ac123071b58217ae218dcbab
Logan Smith <logansmith@my.whccd.edu>
2023-03-09 23:34:54 -0800

    Changes 3-9-23

--- COMP003A.FinalExam/Program.cs
+++ COMP003A.FinalExam/Program.cs
@@ -8,7 +8,7 @@ namespace COMP003A.FinalExam
 {
     internal class Program
     {
-        /*
+        
         static void Main(string[] args)
         {
             Logo(true);
@@ -23,11 +23,32 @@ namespace COMP003A.FinalExam
             Applicant.LastName = Inputty("name");
 
             Console.Write("\nWhat year were you born: ");
-            //Applicant.BirthYear = Convert.ToInt32(Inputty("birthyear"));
+            Applicant.BirthYear = Convert.ToInt32(Inputty("birthyear"));
 
-            Console.WriteLine(Applicant.FirstName);
-            Console.WriteLine(Applicant.LastName);
-           // Console.WriteLine(Applicant.BirthYear);
+            Console.WriteLine("\nWhat is your gender?");
+            Console.Write("Please type F (female), M (male), N (nonbinary), G (genderfluid), O  (other): ");
+            Applicant.Gender = Convert.ToChar(Inputty("gender"));
+
+            Console.Write("\nWhat is your mailing address: ");
+            Applicant.MailingAddress = Inputty("default");
+
+            Console.Write("\nWhat is your City: ");
+            Applicant.City = Inputty("default");
+
+            Console.WriteLine("\nWhat is your state: ");
+            Console.Write("Please input a two letter abbreviation: ");
+            Applicant.State = Inputty("state");
+
+
+            Console.Write("\nWhat is your email address: ");
+            Applicant.EmailAddress = Inputty("email");
+
+            Console.Write("\nWhat is your phone number: ");
+            Applicant.PhoneNumber = Inputty("phone");
+
+
+
+
 
 
 
@@ -35,92 +56,58 @@ namespace COMP003A.FinalExam
         }
 
 
+        ///<summary>
+        ///Method to handle input and input validation
+        ///</summary>
+        ///<param name="type">Type of input (email, name, phone, gender, birthyear, state, or else for other)</param>
+        ///<returns>Validated string input</returns>
         static string Inputty(string type)
         {
             string realVar = null;
             int i = 0;
-            string badChars = null;
-            int z = 0;
+            string tempVar = null;
+            
 
 
             while (i == 0)
             {
-                string tempVar = Console.ReadLine();
-
-                switch (type)
-                {
-                    case "email":
-                        badChars = @"\|!#$%&/()@=?*+»«@£§€{}.-;'<>_,";
-                        break;
-                    case "name":
-                        badChars = @"1234567890\|!#$%&/()@.=?*+»«@£§€{}-;'<>_,";
-                        break;
-                    case "phone":
-                        badChars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRUTUVWXYZ\|!#$%&/()@.=?+»«@£§€{};'<>_,";
-                        break;
-                    default:
-                        badChars = @"\|!$%&/()@=?*+»«@£§€{};'<>";
-                        break;
-                }
-
-
-
-
-
-
-                /*
+                int z = 0;
+                //switch statement to handle data validation   
+                tempVar = Console.ReadLine();        
                 switch(type)
                 {
                     case "email":
-                        tempVar = Console.ReadLine();
-                        string badChars = @"\|!#$%&/()@=?*+»«@£§€{}.-;'<>_,";
-                        foreach(var item in badChars)
+                        string badChars = @"\|!#$%&/()=?*+»«£§€{};'<>_,";
+                        foreach(var item in badChars) // tests if the inputted string has any of the bad characters
                         {
-                            if ((tempVar.Contains(item)))
+                            if ((tempVar.Contains(item)) || !ConvertTest(tempVar, "string"))
                             {
                                 z++;
                             }
-
-                            if(z == 0)
-                            {
-                                realVar = tempVar;
-                                i = 1;
-                            }
-                            else
-                            {
-                                Console.Write("\nSorry, try again: ");
-                            }
-
+                            
                         }
                         break;
 
 
                     case "name":
-                        tempVar = Console.ReadLine();
-                        badChars = @"1234567890\|!#$%&/()@.=?*+»«@£§€{}-;'<>_,";
+                        badChars = @"1234567890\|!#$%&/().=?*+»«@£§€{}-;'<>_,";
                         foreach (var item in badChars)
                         {
                             if ((tempVar.Contains(item)))
                             {
                                 z++;
-                            }
-
-                            if (z == 0)
-                            {
-                                realVar = tempVar;
-                                i = 1;
-                            }
-                            else
-                            {
-                                Console.Write("\nSorry, try again: ");
-                            }
+                            }                           
+                        }
+                        if(!ConvertTest(tempVar, "string"))
+                        {
+                            z++;
                         }
                         break;
 
 
                     case "phone":
-                        tempVar = Console.ReadLine();
-                        badChars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRUTUVWXYZ\|!#$%&/()@.=?+»«@£§€{};'<>_,";
+                       // tempVar = Console.ReadLine();
+                        badChars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRUTUVWXYZ\|!#$%&/.=?+»«@£§€{};'<>_,";
                         foreach (var item in badChars)
                         {
                             if ((tempVar.Contains(item)))
@@ -128,57 +115,75 @@ namespace COMP003A.FinalExam
                                 z++;
                             }
 
-                            if (z == 0)
-                            {
-                                realVar = tempVar;
-                                i = 1;
-                            }
-                            else
-                            {
-                                Console.Write("\nSorry, try again: ");
-                                tempVar = null;
-                            }
                         }
                         break;
 
-                    case "birthyear":
-                        try
+                    case "state":
+                        int position = 0;
+                        List<string> stateAb = new List<string> {"DNE123", "AI", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC", "GU", "MH", "MP", "PR", "VI"};
+                        position = stateAb.IndexOf(tempVar);
+                        if(position > 0)
                         {
-                            Convert.ToInt32(tempVar);
-                            i = 1;
+                            realVar = stateAb[position];
                         }
-                        catch (System.FormatException)
+                        else
                         {
-                            Console.Write("\nSorry, try again: ");
-                            tempVar = null;
+                            z++;
+                        }
+
+                        break;
+
+                    case "birthyear":
+                        if(!ConvertTest(tempVar, "int"))
+                        {
+                            z++;
+                        }
+                        else //in else so things don't mess up if input is bad
+                        {
+                            if(Convert.ToInt32(tempVar) <= 1900){ z++;}
+                        }
+                        break;
+
+                    case "gender":
+                        badChars = @"abcdefghijklmnopqrstuvwxyzABCDEHIJKLPQRSTUVWXYZ1234567890\|!#$%&/().=?*+»«@£§€{}-;'<>_,";
+                        if(!ConvertTest(tempVar,"char"))
+                        {
+                            z++;
+                        }
+                        foreach(var item in badChars)
+                        {
+                            if(tempVar.Contains(item))
+                            {
+                                z++;
+                            }
                         }
                         break;
 
 
                     default:
-                        tempVar = Console.ReadLine();
-                        badChars = @"\|!$%&/()@=?*+»«@£§€{};'<>";
+                      //  tempVar = Console.ReadLine();
+                        badChars = @"\|$%&/()@=*+»«£§€{};'<>";
                         foreach (var item in badChars)
                         {
-                            if ((tempVar.Contains(item)))
+                            if ((tempVar.Contains(item)) || !ConvertTest(tempVar, "string"))
                             {
                                 z++;
                             }
-
-                            if (z == 0)
-                            {
-                                realVar = tempVar;
-                                i = 1;
-                            }
-                            else
-                            {
-                                Console.Write("\nSorry, try again: ");
-                                tempVar = null;
-                            }
+                           
                         }
                         break;
                 
             }
+
+            if(z == 0)
+                {
+                realVar = tempVar;
+                i = 1;
+                }
+                else
+                {
+                     Console.Write("\nSorry, try again: ");
+                 }
                 
 
                 
@@ -192,6 +197,41 @@ namespace COMP003A.FinalExam
 
 
 
+        ///<summary>
+        ///Tests if converting string (typically user input) is okay
+        ///Dual purpose - prevents null
+        ///</summary>
+        ///<param name="intake">String input to test against</param>
+        ///<param name="type">String input for type (int, char; string to check against null)</param>
+        ///<returns>Boolean: True (can convert) False (cannot convert)</returns>
+        static bool ConvertTest(string intake, string type)
+        {
+            bool end = true;
+            try
+            {
+                switch (type)
+                {
+                    case "int":
+                    Convert.ToInt32(intake);
+                    break;
+
+                    case "char":
+                    Convert.ToChar(intake);
+                    break;
+
+                    case "string":
+                    if(intake == null) {end = false;}
+                    break;
+                }
+
+            }
+            catch (System.FormatException)
+            {
+                end = false;
+            }
+
+            return end;
+        }
 
 
         ///<summary>
@@ -220,6 +260,6 @@ namespace COMP003A.FinalExam
         }
 
 
-        */
+        
     }
 }
